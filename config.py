8820f0a3cbe8ff06c0080aacce4f7d82db058a19
keingsw <dev.keingsw@gmail.com>
2020-02-06 11:57:57 +0800

    Connect to local database

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
 
+# Connect to the database
+SQLALCHEMY_DATABASE_URI = 'postgres://fyyur_db_user@localhost:5432/fyyur_db'
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
