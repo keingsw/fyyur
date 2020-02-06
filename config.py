f4e8cfbd9e24b6c2919c6749f8d1f2e78a741ae8
keingsw <dev.keingsw@gmail.com>
2020-02-06 11:58:24 +0800

    Add SQLALCHEMY_TRACK_MODIFICATIONS option to config to turn off the warning

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+# Turn off the Flask-SQLAlchemy event system and warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'postgres://fyyur_db_user@localhost:5432/fyyur_db'
