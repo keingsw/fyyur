ec02d3f044e99127501ee71fe1dac934a75f63c8
keingsw <dev.keingsw@gmail.com>
2020-02-07 12:41:52 +0800

    Add NOT NULL constraint to `shows.start_time`

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ class Show(db.Model):
     __tablename__ = 'shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime)
+    start_time = db.Column(db.DateTime, nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
 
