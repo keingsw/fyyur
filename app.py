7e88b658247b5ccb33e933e2a9fcda3c868cf1d3
keingsw <dev.keingsw@gmail.com>
2020-02-07 12:28:52 +0800

    Implement form submission for creating new Artists

--- app.py
+++ app.py
@@ -613,14 +613,25 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-    # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
+    artist = Artist(name=request.form.get('name'),
+                    city=request.form.get('city'),
+                    state=request.form.get('state'),
+                    phone=request.form.get('phone'),
+                    facebook_link=request.form.get('facebook_link'))
 
-    # on successful db insert, flash success
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    for genre_name in request.form.getlist('genres'):
+        artist.genres.append(ArtistGenre(genre_name=genre_name))
+
+    try:
+        db.session.add(artist)
+        db.session.commit()
+        flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+
     return render_template('pages/home.html')
 
 
